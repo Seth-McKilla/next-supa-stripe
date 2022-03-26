84985ac3e8390dc4ba70c29f57d5eef7b0627969
Seth McCullough <seth@endevrs.dev>
2022-03-26 17:42:18 -0400

    Reformat stripe customer api route

---
+++ pages/api/stripe_customer.ts
@@ -0,0 +1,42 @@
+import initStripe from "stripe";
+import { NextApiRequest, NextApiResponse } from "next";
+import { supabase } from "../../utils/supabase";
+
+declare const process: {
+  env: {
+    API_ROUTE_SECRET: string;
+    STRIPE_SECRET_KEY: string;
+  };
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.query.API_ROUTE_SECRET !== process.env.API_ROUTE_SECRET) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const stripe = new initStripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2020-08-27",
+    typescript: true,
+  });
+
+  switch (req.method) {
+    case "POST":
+      const customer = await stripe.customers.create({
+        email: req.body.record.email,
+      });
+
+      await supabase
+        .from("profile")
+        .update({ stripe_customer: customer.id })
+        .eq("id", req.body.record.id);
+
+      return res.send({ message: `Stripe customer created: ${customer.id}` });
+
+    default:
+      res.setHeader("Allow", ["POST"]);
+      return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
